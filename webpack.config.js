1cde829b8ef76f93a5e9b264f907e4454d318cf9
Joe Byden <joebyden@example.com>
2024-11-01 02:15:58 +0400

    feat: LAB4-3 Созданы web-страницы с использованием html,css,js, настроен webpack для корректной работы приложения

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,37 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        about: './src/about.js',
+        projects: './src/projects',
+        tasks: '/src/tasks',
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            outputPath: 'assets/',
+                        },
+                    },
+                ],
             },
         ],
     },
@@ -20,7 +40,37 @@ module.exports = {
             template: './src/index.html',
             inject: true,
             chunks: ['index'],
-            filename: 'index.html'
+            filename: 'index.html',
+            favicon: './src/assets/favicon.png',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/about.html',
+            inject: true,
+            chunks: ['about'],
+            filename: 'about.html',
+            favicon: './src/assets/favicon.png',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/projects.html',
+            inject: true,
+            chunks: ['projects'],
+            filename: 'projects.html',
+            favicon: './src/assets/favicon.png',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/tasks.html',
+            inject: true,
+            chunks: ['tasks'],
+            filename: 'tasks.html',
+            favicon: './src/assets/favicon.png',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets'},
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
         }),
     ],
     devServer: {
